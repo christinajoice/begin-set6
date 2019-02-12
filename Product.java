68134d9a4deac3eca50529ee4ff5bd71f762fa1f
christinajoice <christinallrejoice@gmail.com>
2019-02-11 17:41:17 -0800

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+public class Neareven
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+System.out.println("Enter two numbers"):
+int num1=s.nextInt();
+int num2=s.nextInt();
+int prod=num1*num2;
+if(pro%2==0)
+{
+System.out.println("even");
+}
+else
+{
+System.out.println("odd");
+}
+}
+}
